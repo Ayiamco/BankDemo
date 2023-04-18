daeb955a3f1ad4819d4f086ffdef30808d885d01
joseph achonu <josephachonu@gmail.com>
2023-04-18 06:03:48 +0100

    code refactor

--- BE/src/InterBankSettlement.Api/Program.cs
+++ BE/src/InterBankSettlement.Api/Program.cs
@@ -1,27 +1,12 @@
-using BankDemo.Infrastructure.Config;
 using BankDemo.Infrastructure.ExtensionMethods;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddApiVersioning(x =>
-{
-    x.DefaultApiVersion = new ApiVersion(1, 0);
-    x.AssumeDefaultVersionWhenUnspecified = true;
-    x.ReportApiVersions = true;
-});
-builder.Services.AddVersionedApiExplorer(setup =>
-{
-    setup.GroupNameFormat = "'v'VVV";
-    setup.SubstituteApiVersionInUrl = true;
-});
-builder.Services.AddSwaggerGen();
-builder.Services.ConfigureOptions<SwaggerConfigOptions>();
+builder.Services.AddSwaggerDoc();
+
 
 var app = builder.Build();
 
---
+++ BE/src/Shared/BankDemo.Infrastructure/ExtensionMethods/IServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using BankDemo.Infrastructure.Config;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BankDemo.Infrastructure.ExtensionMethods
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSwaggerDoc(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddApiVersioning(x =>
+            {
+                x.DefaultApiVersion = new ApiVersion(1, 0);
+                x.AssumeDefaultVersionWhenUnspecified = true;
+                x.ReportApiVersions = true;
+            });
+            services.AddVersionedApiExplorer(setup =>
+            {
+                setup.GroupNameFormat = "'v'VVV";
+                setup.SubstituteApiVersionInUrl = true;
+            });
+            services.AddSwaggerGen();
+            services.ConfigureOptions<SwaggerConfigOptions>();
+            return services;
+        }
+    }
+}
